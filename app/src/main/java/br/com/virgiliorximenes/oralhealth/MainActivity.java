9fc36f7d162afa63fefb83a67dc375e6f865ac14
virgilio <virgiliorximenes@gmail.com>
2016-08-07 14:39:38 -0400

    Menu Screen

--- app/src/main/java/br/com/virgiliorximenes/oralhealth/MainActivity.java
+++ app/src/main/java/br/com/virgiliorximenes/oralhealth/MainActivity.java
@@ -3,7 +3,7 @@ package br.com.virgiliorximenes.oralhealth;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
-import android.support.v7.app.AppCompatActivity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 
@@ -17,7 +17,45 @@ public class MainActivity extends Activity implements View.OnClickListener {
 
     private void showMain() {
         setContentView(R.layout.activity_main);
+
         findViewById(R.id.menuStart).setOnClickListener(this);
+        findViewById(R.id.about_button).setOnClickListener(this);
+        findViewById(R.id.music_button).setOnClickListener(this);
+        findViewById(R.id.sfx_button).setOnClickListener(this);
+
+        SharedPreferences sharedPreferences = getSharedPreferences(getString(R.string.app_name), MODE_PRIVATE);
+
+        boolean musicEnabled;
+        boolean sfxEnabled;
+        if (sharedPreferences.getInt(getString(R.string.app_name), 0) == 1) {
+            musicEnabled = sharedPreferences.getBoolean(getString(R.string.music), true);
+            sfxEnabled = sharedPreferences.getBoolean(getString(R.string.sfx), true);
+
+        } else {
+            musicEnabled = true;
+            sfxEnabled = true;
+
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            editor.putBoolean(getString(R.string.music), musicEnabled);
+            editor.putBoolean(getString(R.string.sfx), sfxEnabled);
+            editor.putInt(getString(R.string.app_name), 1);
+            editor.commit();
+
+        }
+
+        if (musicEnabled) {
+            findViewById(R.id.music_button).setOnClickListener(this);
+        } else {
+            findViewById(R.id.music_button).setOnClickListener(this);
+        }
+
+        if (sfxEnabled) {
+            findViewById(R.id.sfx_button).setOnClickListener(this);
+        } else {
+            findViewById(R.id.sfx_button).setOnClickListener(this);
+        }
+
+
     }
 
     @Override
