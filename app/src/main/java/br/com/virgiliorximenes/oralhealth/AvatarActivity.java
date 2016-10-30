4c747c6bb66bca25d079227b542a29e528aec620
virgilio <virgiliorximenes@gmail.com>
2016-10-30 12:09:35 -0400

    Some stuff.

--- app/src/main/java/br/com/virgiliorximenes/oralhealth/AvatarActivity.java
+++ app/src/main/java/br/com/virgiliorximenes/oralhealth/AvatarActivity.java
@@ -21,11 +21,13 @@ public class AvatarActivity extends Activity implements View.OnClickListener {
     @Override
     public void onClick(View view) {
         if (R.id.gender_male == view.getId()) {
-            ((ImageView) findViewById(R.id.avatar_image))
-                    .setImageResource(R.drawable.avatar_boy);
+            ImageView imageView = (ImageView) findViewById(R.id.avatar_image);
+            imageView.setImageResource(R.drawable.avatar_boy);
+            imageView.setContentDescription(getString(R.string.gender_male));
         } else if (R.id.gender_female == view.getId()) {
-            ((ImageView) findViewById(R.id.avatar_image))
-                    .setImageResource(R.drawable.avatar_girl);
+            ImageView imageView = (ImageView) findViewById(R.id.avatar_image);
+            imageView.setImageResource(R.drawable.avatar_girl);
+            imageView.setContentDescription(getString(R.string.gender_female));
         } else if (R.id.choose_avatar == view.getId()) {
             Toast.makeText(getApplicationContext(), "Not implement yet!", Toast.LENGTH_SHORT).show();
         }
